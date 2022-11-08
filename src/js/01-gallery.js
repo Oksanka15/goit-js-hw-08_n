cf9153a71c5be03cbaccefe642332dfa45598261
Oksanka15 <dzobaoksana1993@gmail.com>
2022-11-08 13:54:05 +0100

    first comit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,22 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
 
-import SimpleLightbox from "simplelightbox";
+const galleryDiv = document.querySelector(".gallery");
+const galleryCard = createGalleryCards(galleryItems);
+galleryDiv.insertAdjacentHTML('afterbegin', galleryCard);
+
+function createGalleryCards(images) {
+    return images.map(image => 
+`<a class="gallery__item" href="${image.original}" onclick="return false;">
+<img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+).join('');
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt"
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const ifframe = document.querySelector('#video-player');
+const player = new Player(ifframe);
+console.log(player);
+
+const onPlayer = ({ seconds }) =>
+    {
+        localStorage.setItem('videoplayer-current-time', seconds)
+    };
+    if(localStorage.getItem('videoplayer-current-time')){
+        player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+    }
+    player.on('timeupdate',throttle(onPlayer,1000))
